9fb9f1be71bec2b96adb85b0d38dab8916aabc89
jk1 <eugenbox@gmail.com>
2016-07-18 17:53:37 +0300

    #3 basic issue preview

--- src/main/kotlin/com/github/jk1/ytplugin/search/IssueViewer.kt
+++ src/main/kotlin/com/github/jk1/ytplugin/search/IssueViewer.kt
@@ -8,60 +8,68 @@ import com.intellij.openapi.Disposable
 import com.intellij.openapi.actionSystem.*
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.util.Ref
 import com.intellij.ui.CollectionListModel
 import com.intellij.ui.components.JBList
 import com.intellij.ui.components.JBLoadingPanel
 import com.intellij.ui.components.JBScrollPane
+import com.intellij.ui.components.JBScrollPane.HORIZONTAL_SCROLLBAR_NEVER
+import com.intellij.ui.components.JBScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED
 import java.awt.BorderLayout
 import java.awt.event.ComponentAdapter
 import java.awt.event.ComponentEvent
+import javax.swing.JComponent
 
-class IssueViewer(val project: Project, parent: Disposable) : JBLoadingPanel(BorderLayout(), parent), DataProvider, Disposable {
+class IssueViewer(val project: Project, parent: Disposable) : JBLoadingPanel(BorderLayout(), parent), DataProvider {
 
-    var myList: JBList = JBList()
+    var issueList: JBList = JBList()
 
     init {
-        myList.fixedCellHeight = 80
-        myList.cellRenderer = IssueListCellRenderer()
-        myList.model = CollectionListModel<Issue>()
-
         val splitter = EditorSplitter(project)
-
         val browser = MessageBrowser(project)
-
-        val scrollPane = JBScrollPane(myList, JBScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JBScrollPane.HORIZONTAL_SCROLLBAR_NEVER)
+        issueList.fixedCellHeight = 80
+        issueList.cellRenderer = IssueListCellRenderer()
+        issueList.model = CollectionListModel<Issue>()
+        issueList.addListSelectionListener {
+            browser.showIssue(issueList.model.getElementAt(it.firstIndex) as Issue)
+        }
+        val scrollPane = JBScrollPane(issueList, VERTICAL_SCROLLBAR_AS_NEEDED, HORIZONTAL_SCROLLBAR_NEVER)
         scrollPane.addComponentListener(object : ComponentAdapter() {
-            override fun componentResized(e: ComponentEvent?) {
-                myList.fixedCellWidth = scrollPane.visibleRect.width - 30
+            override fun componentResized(e: ComponentEvent) {
+                issueList.fixedCellWidth = scrollPane.visibleRect.width - 30
             }
         })
 
         splitter.firstComponent = scrollPane
         splitter.secondComponent = browser
-
         add(splitter, BorderLayout.CENTER)
+        add(createActionPanel(), BorderLayout.WEST)
 
-        startLoading()
-        ApplicationManager.getApplication().executeOnPooledThread {
-            val model = Ref.create(myList.model)
-            try {
-                model.set(IssuesModel(project))
-            } finally {
-                ApplicationManager.getApplication().invokeLater {
-                    myList.model = model.get()
-                    stopLoading()
-                }
-            }
-        }
+        initModel()
+    }
+
+    private fun createActionPanel(): JComponent{
         val group = DefaultActionGroup()
         group.add(RefreshIssuesAction())
         group.add(CreateIssueAction())
         group.add(SetAsActiveTaskAction())
-        add(ActionManager.getInstance()
+        return ActionManager.getInstance()
                 .createActionToolbar("Actions", group, false)
-                .component, BorderLayout.WEST)
+                .component
+    }
 
+    private fun initModel(){
+        startLoading()
+        ApplicationManager.getApplication().executeOnPooledThread {
+            var model = issueList.model
+            try {
+                model = IssuesModel(project)
+            } finally {
+                ApplicationManager.getApplication().invokeLater {
+                    issueList.model = model
+                    stopLoading()
+                }
+            }
+        }
     }
 
     override fun getData(dataId: String): Any? {
@@ -69,18 +77,13 @@ class IssueViewer(val project: Project, parent: Disposable) : JBLoadingPanel(Bor
             return project
         }
         if (DataKey.create<Array<Issue>>("MYY_ISSUES_ARRAY").equals(dataId)) {
-            val values = myList.selectedValuesList
+            val values = issueList.selectedValuesList
             val issues = arrayOfNulls<Issue>(values.size)
             for (i in values.indices) {
                 issues[i] = values[i] as Issue
             }
             return issues
         }
-
         return null
-    }
-
-    override fun dispose() {
-
     }
 }
