801cc252a637150d23dd1d19024319d329864bcc
jk1 <eugenbox@gmail.com>
2017-09-10 11:15:32 +0300

    #68 plugin api to be used by other plugins

--- api/src/main/java/com/github/jk1/ytplugin/YouTrackPluginApi.java
+++ api/src/main/java/com/github/jk1/ytplugin/YouTrackPluginApi.java
@@ -3,7 +3,17 @@ package com.github.jk1.ytplugin;
 
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * YouTrack integration plugin API to be used by other plugins
+ */
 interface YouTrackPluginApi {
 
+    /**
+     * Open issue preview in YouTrack tool window.
+     * This method shouldn't be called from EDT as it may include networking.
+     *
+     * @param issueId logical issue id to open, e.g. TW-1234
+     * @throws IllegalArgumentException if target issue is nowhere to be found
+     */
     void openIssueInToolWidow(@NotNull String issueId);
 }
--- src/main/kotlin/com/github/jk1/ytplugin/issues/PersistentIssueStoreComponent.kt
+++ src/main/kotlin/com/github/jk1/ytplugin/issues/PersistentIssueStoreComponent.kt
@@ -16,15 +16,21 @@ import java.util.concurrent.ConcurrentHashMap
  * a project directory. This comes in handy for projects with /.idea under VCS control.
  */
 @State(name = "YouTrack Issues", storages = arrayOf(Storage(value = "issues.xml")))
-class PersistentIssueStoreComponent() : ApplicationComponent, PersistentStateComponent<Memento> {
+class PersistentIssueStoreComponent : ApplicationComponent, PersistentStateComponent<Memento> {
 
     private var loadedMemento: Memento = Memento()
     private val stores = ConcurrentHashMap<String, IssueStore>()
 
+    override fun getComponentName(): String = javaClass.name
+
+    override fun disposeComponent() {}
+
+    override fun initComponent() {}
+
     override fun getState() = Memento(stores)
 
-    override fun loadState(state: Memento?) {
-        loadedMemento = state!!
+    override fun loadState(state: Memento) {
+        loadedMemento = state
     }
 
     operator fun get(repo: YouTrackServer): IssueStore {
@@ -35,28 +41,27 @@ class PersistentIssueStoreComponent() : ApplicationComponent, PersistentStateCom
     }
 
     fun remove(repo: YouTrackServer) {
-       stores.remove(repo.id)
+        stores.remove(repo.id)
     }
 
     class Memento constructor() {
 
         // implementation should stay mutable as deserializer calls #clear() on it
-        var persistentIssues: Map<String, String> = mutableMapOf()
+        private var persistentIssues: Map<String, String> = mutableMapOf()
 
         // primary constructor is reserved for serializer
         constructor(stores: Map<String, IssueStore>) : this() {
-            persistentIssues = stores.mapValues { "[${it.value.map { it.json }.joinToString(", ")}]" }
+            persistentIssues = stores.mapValues { "[${it.value.joinToString(", ") { it.json }}]" }
         }
 
         fun getStore(repo: YouTrackServer): IssueStore {
             try {
                 val issuesJson = persistentIssues[repo.id] ?: return IssueStore()
                 val issues = JsonParser().parse(issuesJson).asJsonArray
-                        .map { IssueJsonParser.parseIssue(it, repo.url) }
-                        .filterNotNull()
+                        .mapNotNull { IssueJsonParser.parseIssue(it, repo.url) }
                 logger.debug("Issue store file cache loaded for ${repo.url} with a total of ${issues.size}")
                 return IssueStore(issues)
-            } catch(e: Exception) {
+            } catch (e: Exception) {
                 logger.warn("Failed to load issue store file cache for ${repo.url}", e)
                 return IssueStore()
             }
