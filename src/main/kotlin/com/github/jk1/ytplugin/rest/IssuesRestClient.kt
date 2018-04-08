37f657b1a76a7beb872450544f1d5039aa601ae7
jk1 <eugenbox@gmail.com>
2018-04-08 16:42:31 +0200

    Avoid heavy search requests to the server

--- src/main/kotlin/com/github/jk1/ytplugin/rest/IssuesRestClient.kt
+++ src/main/kotlin/com/github/jk1/ytplugin/rest/IssuesRestClient.kt
@@ -36,7 +36,7 @@ class IssuesRestClient(override val repository: YouTrackServer) : RestClientTrai
             val issuesIds = issues.map { it.id }
             val projects = issuesIds.groupBy { it.split("-")[0] }
             val wikifiedIssues = projects.flatMap {
-                val issueIdsQuery = it.value.joinToString(prefix = "#", separator = ", ")
+                val issueIdsQuery = it.value.joinToString(" ") { "#$it" }
                 getWikifiedIssuesInProject(it.key, issueIdsQuery)
             }
             return issuesIds.mapNotNull { id -> wikifiedIssues.firstOrNull { issue -> id == issue.id } }
