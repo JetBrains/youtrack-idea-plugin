73075860141d21da7be8cc3b53831b1c26139afa
AlinaBoshchenko <alina_boshenko@mail.ru>
2020-08-25 23:04:56 +0300

    #bugfix patch-1

---
+++ src/main/kotlin/com/github/jk1/ytplugin/setup/ConnectionChecker.kt
@@ -0,0 +1,72 @@
+package com.github.jk1.ytplugin.setup
+
+import com.github.jk1.ytplugin.logger
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.tasks.youtrack.YouTrackRepository
+import org.apache.commons.httpclient.HttpMethod
+import org.apache.commons.httpclient.methods.PostMethod
+
+class ConnectionChecker(val repository: YouTrackRepository) {
+    @Volatile
+    private var onSuccess: () -> Unit = {}
+
+    @Volatile
+    private var onApplicationError: (method: HttpMethod, responseCode: Int) -> Unit = { _: HttpMethod, _: Int -> }
+
+    @Volatile
+    private var onTransportError: (method: HttpMethod, e: Exception) -> Unit = { _: HttpMethod, _: Exception -> }
+
+    fun check() {
+        doConnect()
+//            ApplicationManager.getApplication().executeOnPooledThread {
+//                doConnect()
+//            }
+    }
+
+    private fun doConnect() {
+        logger.debug("CHECK CONNECTION FOR ${repository.url}")
+
+        val method = PostMethod(repository.url.trimEnd('/') + "/api/token")
+        method.setRequestHeader("Authorization", "Bearer " + repository.password)
+        try {
+            repository.httpClient.executeMethod(method)
+            if (method.statusCode == 200) {
+                logger.debug("connection status: SUCCESS")
+                onSuccess()
+            } else {
+                logger.debug("connection status: APPLICATION ERROR")
+                onApplicationError(method, method.statusCode)
+            }
+        } catch (e: Exception) {
+            logger.debug("connection status: TRANSPORT ERROR")
+            onTransportError(method, e)
+        } finally {
+            logger.debug("connection status: RELEASED")
+            method.releaseConnection()
+        }
+    }
+
+    fun onSuccess(closure: () -> Unit) {
+        this.onSuccess = {
+            ApplicationManager.getApplication().invokeLater {
+                closure.invoke()
+            }
+        }
+    }
+
+    fun onApplicationError(closure: (method: HttpMethod, responseCode: Int) -> Unit) {
+        this.onApplicationError = { method: HttpMethod, responseCode: Int ->
+            ApplicationManager.getApplication().invokeLater {
+                closure.invoke(method, responseCode)
+            }
+        }
+    }
+
+    fun onTransportError(closure: (method: HttpMethod, e: Exception) -> Unit) {
+        this.onTransportError = { method: HttpMethod, e: Exception ->
+            ApplicationManager.getApplication().invokeLater {
+                closure.invoke(method, e)
+            }
+        }
+    }
+}
--- src/main/kotlin/com/github/jk1/ytplugin/setup/SetupRepositoryConnector.kt
+++ src/main/kotlin/com/github/jk1/ytplugin/setup/SetupRepositoryConnector.kt
@@ -1,15 +1,11 @@
 package com.github.jk1.ytplugin.setup
 
 import com.github.jk1.ytplugin.logger
-import com.github.jk1.ytplugin.setup.connection.CancellableConnection
-import com.github.jk1.ytplugin.setup.connection.HttpTestConnection
-import com.github.jk1.ytplugin.setup.connection.TestConnectionTask
 import com.google.gson.JsonObject
 import com.google.gson.JsonParser
-import com.intellij.openapi.application.ApplicationManager
-import com.intellij.openapi.progress.ProcessCanceledException
 import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.progress.ProgressManager
+import com.intellij.openapi.progress.Task
 import com.intellij.openapi.project.Project
 import com.intellij.tasks.TaskManager
 import com.intellij.tasks.TaskRepository
@@ -17,52 +13,24 @@ import com.intellij.tasks.config.RecentTaskRepositories
 import com.intellij.tasks.impl.TaskManagerImpl
 import com.intellij.tasks.youtrack.YouTrackRepository
 import com.intellij.util.containers.ContainerUtil
-import com.intellij.util.io.HttpRequests
 import org.apache.commons.httpclient.HttpClient
+import org.apache.commons.httpclient.HttpMethod
 import org.apache.commons.httpclient.NameValuePair
 import org.apache.commons.httpclient.URI
-import org.apache.commons.httpclient.UsernamePasswordCredentials
-import org.apache.commons.httpclient.auth.AuthScope
 import org.apache.commons.httpclient.methods.GetMethod
-import org.apache.commons.httpclient.methods.PostMethod
 import java.awt.Color
 import java.io.InputStreamReader
-import java.net.UnknownHostException
 import java.nio.charset.StandardCharsets
+import java.util.concurrent.CompletableFuture
+import java.util.concurrent.Future
 import java.util.concurrent.TimeUnit
 import java.util.concurrent.TimeoutException
+import javax.net.ssl.SSLException
 import javax.swing.JLabel
 
 class SetupRepositoryConnector {
-
-    var correctUrl: String = ""
+    var correctUrl: String = "" // todo: ?
     var noteState = NotifierState.INVALID_TOKEN
-    var isEndingFixed = false
-    var statusCode = 401
-
-    fun getRepositoryUsername(repository: YouTrackRepository): String? {
-        return repository.username
-    }
-
-    fun getRepositoryPassword(repository: YouTrackRepository): String? {
-        return repository.password
-    }
-
-    fun getRepositoryUrl(repository: YouTrackRepository): String? {
-        return trimTrailingSlashes(repository.url)
-    }
-
-
-    private fun trimTrailingSlashes(url: String?): String? {
-        if (url == null) return ""
-        for (i in url.length - 1 downTo 0) {
-            if (url[i] != '/') {
-                return url.substring(0, i + 1)
-            }
-        }
-        return ""
-    }
-
     private fun isValidToken(token: String): Boolean {
         val tokenPattern = Regex("perm:([^.]+)\\.([^.]+)\\.(.+)")
         return token.matches(tokenPattern)
@@ -80,187 +48,150 @@ class SetupRepositoryConnector {
             NotifierState.UNKNOWN_HOST -> note.text = "Unknown host"
             NotifierState.INVALID_TOKEN -> note.text = "Invalid token"
             NotifierState.NULL_PROXY_HOST -> note.text = "Invalid proxy host"
+            NotifierState.INCORRECT_CERTIFICATE -> note.text = "Incorrect certificate"
+            NotifierState.TIMEOUT -> note.text = "Connection timeout: check login and token"
+            NotifierState.UNAUTHORIZED -> note.text = "Unauthorized: check login and token"
+            NotifierState.CONNECTION_ERROR -> note.text = "Connection error, maybe you are offline?"
             NotifierState.INVALID_VERSION -> note.text = "<html>Incompatible YouTrack version,<br/>please update to 2017.1</html>"
         }
     }
 
-
-    private fun fixEnding(uri: String): String {
-        var newUri = uri
-        if (!uri.contains("/youtrack")) {
-            newUri = "$newUri/youtrack"
-        }
-        isEndingFixed = true
-        return newUri
-    }
-
-    private fun fixProtocol(oldUrl: URI): URI {
-        val url = URI("https",null, oldUrl.host, oldUrl.port, oldUrl.path, oldUrl.query, oldUrl.fragment)
-
-        return url
-    }
-
     private fun isValidYouTrackVersion(repository: YouTrackRepository): Boolean {
         val client = HttpClient()
-        val method = GetMethod(getRepositoryUrl(repository) + "/api/config")
+        val method = GetMethod(repository.url.trimEnd('/') + "/api/config")
         val fields = NameValuePair("fields", "version")
         method.setQueryString(arrayOf(fields))
         method.setRequestHeader("Authorization", "Bearer " + repository.password)
-        val status = client.executeMethod(method)
-
-        return if (status == 200) {
-            val reader = InputStreamReader(method.responseBodyAsStream, StandardCharsets.UTF_8)
-            val json: JsonObject = JsonParser.parseReader(reader).asJsonObject
-            if (json.get("version") == null || json.get("version").isJsonNull) {
-                noteState = NotifierState.LOGIN_ERROR
-                false
-            } else {
-                (json.get("version").asString.toDouble() >= 2017.1)
-            }
-        }
-        else {
-            noteState = NotifierState.LOGIN_ERROR
-            logger.warn("INVALID LOGIN OR TOKEN, FAILED ON VERSION VALIDATION: ${method.statusCode}")
-            false
-        }
-    }
-
-
-    @Throws(java.lang.Exception::class)
-    private fun login(method: PostMethod, repository: YouTrackRepository): HttpClient? {
-        val client = repository.httpClient
-        val credentials = UsernamePasswordCredentials(getRepositoryUsername(repository), getRepositoryPassword(repository))
-        client.state.setCredentials(AuthScope.ANY, credentials)
-        method.addParameter("login", getRepositoryUsername(repository))
-        method.addParameter("password", getRepositoryPassword(repository))
-        client.params.contentCharset = "UTF-8"
-        client.executeMethod(method)
 
         try {
-            statusCode = method.statusCode
-            when {
-                statusCode in 301..399 -> {
-                    if (isEndingFixed || repository.url.contains("/youtrack")){
-                        val tmp = method.uri
-                        val new = URI(tmp.toString().substring(0, tmp.toString().length - 10), false)
-                        val newUri = fixProtocol(new)
-                        method.uri = URI("https://alinaboshchenko.myjetbrains.com/youtrack", false)
-                        repository.url = newUri.toString()
-                        correctUrl = repository.url
-                    }
-                    else{
-                        val newUri = fixEnding(repository.url)
-                        method.uri = URI(newUri, false)
-                        repository.url = newUri
-                        correctUrl = repository.url
-                    }
+            val status = client.executeMethod(method)
+            return if (status == 200) {
+                val reader = InputStreamReader(method.responseBodyAsStream, StandardCharsets.UTF_8)
+                val json: JsonObject = JsonParser.parseReader(reader).asJsonObject
+                if (json.get("version") == null || json.get("version").isJsonNull) {
+                    noteState = NotifierState.LOGIN_ERROR
+                    false
+                } else {
+                    (json.get("version").asString.toDouble() >= 2017.1)
+                }
+            } else {
+                noteState = NotifierState.LOGIN_ERROR
+                logger.warn("invalid token or login, failed on version validation: ${method.statusCode}")
+                false
+            }
+        } catch (e: Exception) {
+            logger.warn("invalid token or login, failed on version validation: ${method.statusCode}")
+        }
+        return false
+    }
 
-                    createCancellableConnection(repository)
+    private fun checkAndFixConnection(repository: YouTrackRepository): Future<*> {
+        val future = CompletableFuture<Any>()
+        val checker = ConnectionChecker(repository)
+
+        checker.onSuccess {
+            noteState = if (isValidYouTrackVersion(repository)) {
+                logger.debug("valid YouTrack version detected")
+                NotifierState.SUCCESS
+            } else {
+                logger.debug("invalid YouTrack version detected")
+                NotifierState.INVALID_VERSION
+            }
+            future.complete(Unit)
+        }
+        checker.onApplicationError { method, code ->
+            logger.debug("handling application error for ${repository.url}")
+            when (code) {
+                in 301..399 -> {
+                    logger.debug("handling response code 301..399 for the ${repository.url}: REDIRECT")
+                    val location: String = method.getResponseHeader("Location").toString()
+                    // received: "Location: {new_repository.url}/api/token", thus needs to be cut
+                    repository.url = location.substring(10, location.length - 11)
+                    correctUrl = repository.url
+                    logger.debug("url after correction: ${repository.url}")
+                    checker.check()
                 }
-                statusCode == 403 -> {
-                    throw java.lang.Exception("Cannot login: token error occurred or user was banned")
+                403 -> {
+                    logger.debug("handling response code 403 for the ${repository.url}: UNAUTHORIZED")
+                    noteState = NotifierState.UNAUTHORIZED
+                    future.complete(Unit)
                 }
-                statusCode != 200 -> {
-                    println("hrr")
-                    noteState == NotifierState.LOGIN_ERROR
-                    throw HttpRequests.HttpStatusException("Cannot login: incorrect URL or token", method.statusCode, method.path)
+                else -> {
+                    logger.debug("handling response code other than 301..399, 403 ${repository.url}: MANUAL FIX")
+                    if (!method.uri.path.contains("/youtrack")){
+                        repository.url = "${repository.url}/youtrack"
+                        correctUrl = repository.url
+                        logger.debug("url after manual ending fix: ${repository.url}")
+                        checker.check()
+                    } else {
+                        logger.debug("no manual ending fix: LOGIN_ERROR")
+                        noteState = NotifierState.LOGIN_ERROR
+                        future.complete(Unit)
+                    }
                 }
             }
-            method.getResponseBodyAsString(1000) ?: throw IllegalStateException("No response received")
-        } finally {
-            method.releaseConnection()
         }
-        return client
+        checker.onTransportError { method: HttpMethod, exception: Exception ->
+            logger.debug("handling transport error for ${repository.url}")
+
+            when (exception) {
+                is SSLException -> {
+                    logger.debug("application error: INCORRECT_CERTIFICATE")
+                    noteState = NotifierState.INCORRECT_CERTIFICATE
+                    future.complete(Unit)
+                }
+                else -> {
+                    if (method.uri.scheme != "https") {
+                        logger.debug("handling transport error for ${repository.url}: MANUAL PROTOCOL FIX")
+                        val repoUri = URI(repository.url, false)
+                        repository.url = if (repoUri.port != -1){
+                            "https://" + repoUri.host + repoUri.port + repoUri.path
+                        } else {
+                            "https://" + repoUri.host  + repoUri.path
+                        }
+                        correctUrl = repository.url
+                        logger.debug("url after manual protocol fix: ${repository.url}")
+                        checker.check()
+                    } else {
+                        logger.debug("no manual transport fix: LOGIN_ERROR")
+                        noteState = NotifierState.CONNECTION_ERROR
+                        future.complete(Unit)
+                    }
+                }
+            }
+        }
+        checker.check()
+        return future
     }
 
 
-    fun createCancellableConnection(repository: YouTrackRepository): CancellableConnection? {
-        correctUrl = repository.url
-
-        val method = PostMethod(getRepositoryUrl(repository) + "/api/token")
-        method.setRequestHeader("Authorization", "Bearer " + repository.password)
-
-        return object : HttpTestConnection<PostMethod?>(method) {
-            @Throws(java.lang.Exception::class)
-            override fun doTest(method: PostMethod?) {
-                if (method != null) {
-                    login(method, repository)
-                }
-            }
-        }
-    }
-
-
-    fun testConnection(repository: YouTrackRepository, myProject: Project): Boolean {
+    fun testConnection(repository: YouTrackRepository, myProject: Project) {
+        logger.debug("TRY CONNECTION FOR ${repository.url}")
 
         if (!repository.isLoginAnonymously && !isValidToken(repository.password)) {
             noteState = NotifierState.INVALID_TOKEN
-            return false
         }
-
-        val myBadRepositories = ContainerUtil.newConcurrentSet<YouTrackRepository>()
-
-        val task: TestConnectionTask = object : TestConnectionTask("Test connection", myProject) {
+        val task = object : Task.Modal(myProject, "Test connection", true) {
             override fun run(indicator: ProgressIndicator) {
                 indicator.text = "Connecting to " + repository.url + "..."
                 indicator.fraction = 0.0
                 indicator.isIndeterminate = true
                 correctUrl = repository.url
+                val future = checkAndFixConnection(repository)
                 try {
-                    myConnection = createCancellableConnection(repository)
-                    if (myConnection != null) {
-                        val future = ApplicationManager.getApplication().executeOnPooledThread(myConnection!!)
-                        while (true) {
-                            try {
-                                myException = future[100, TimeUnit.MILLISECONDS]
-                                noteState = NotifierState.LOGIN_ERROR
-                                return
-                            } catch (ignore: TimeoutException) {
-                                try {
-                                    indicator.checkCanceled()
-                                } catch (e: ProcessCanceledException) {
-                                    myException = e
-                                    noteState = NotifierState.LOGIN_ERROR
-                                    myConnection!!.cancel()
-                                    return
-                                }
-                            } catch (e: Exception) {
-                                myException = e
-                                noteState = NotifierState.LOGIN_ERROR
-                                return
-                            }
-                        }
-                    }
-                } catch (e: Exception) {
-                    myException = e
+                    future.get(15, TimeUnit.SECONDS)
+                } catch (ignore: TimeoutException) {
+                    logger.debug("could not connect to ${repository.url}: CONNECTION TIMEOUT")
+                    noteState = NotifierState.TIMEOUT
+                    indicator.stop()
                 }
             }
         }
-
         ProgressManager.getInstance().run(task)
-        val e = task.myException
-        if (e == null) {
-            myBadRepositories.remove(repository)
-            correctUrl = repository.url
-            if (isValidYouTrackVersion(repository)) {
-                noteState = NotifierState.SUCCESS
-            }
-            else
-                if (noteState != NotifierState.LOGIN_ERROR)
-                    noteState = NotifierState.INVALID_VERSION
-        } else if (e !is ProcessCanceledException) {
-            val message = e.message
-            if (e is UnknownHostException) {
-                noteState = NotifierState.UNKNOWN_HOST
-            }
-            if (message == null) {
-                noteState = NotifierState.UNKNOWN_ERROR
-            }
-        }
-        return e == null
     }
 
     fun showIssuesForConnectedRepo(repository: YouTrackRepository, project: Project) {
+        logger.debug("showing issues for ${repository.url}...")
         val myManager: TaskManagerImpl = TaskManager.getManager(project) as TaskManagerImpl
         lateinit var myRepositories: List<YouTrackRepository>
         myRepositories = arrayListOf(repository)
@@ -274,7 +205,16 @@ class SetupRepositoryConnector {
 /**
  * Login errors classification
  */
-enum class NotifierState{
-    SUCCESS, LOGIN_ERROR, UNKNOWN_ERROR, UNKNOWN_HOST, INVALID_TOKEN, INVALID_VERSION, NULL_PROXY_HOST
+enum class NotifierState {
+    SUCCESS,
+    LOGIN_ERROR,
+    UNKNOWN_ERROR,
+    UNKNOWN_HOST,
+    INVALID_TOKEN,
+    INVALID_VERSION,
+    NULL_PROXY_HOST,
+    TIMEOUT,
+    INCORRECT_CERTIFICATE,
+    UNAUTHORIZED,
+    CONNECTION_ERROR
 }
-
