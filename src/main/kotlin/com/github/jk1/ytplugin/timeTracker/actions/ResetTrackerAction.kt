8f70a404b7f5e14c770e5f9e85c8e5b7a32a8af4
AlinaBoshchenko <alina_boshenko@mail.ru>
2020-09-05 14:09:49 +0300

    #131 fixed reset timer functionality

--- src/main/kotlin/com/github/jk1/ytplugin/timeTracker/actions/ResetTrackerAction.kt
+++ src/main/kotlin/com/github/jk1/ytplugin/timeTracker/actions/ResetTrackerAction.kt
@@ -1,19 +1,11 @@
 package com.github.jk1.ytplugin.timeTracker.actions
 
-import com.github.jk1.ytplugin.ComponentAware
 import com.github.jk1.ytplugin.issues.actions.IssueAction
-import com.github.jk1.ytplugin.rest.IssuesRestClient
-import com.github.jk1.ytplugin.tasks.YouTrackServer
 import com.github.jk1.ytplugin.timeTracker.TimeTracker
 import com.github.jk1.ytplugin.timeTracker.TrackerNotification
-import com.github.jk1.ytplugin.timeTracker.TrackerNotifier
 import com.github.jk1.ytplugin.whenActive
-import com.intellij.icons.AllIcons
 import com.intellij.notification.NotificationType
 import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.project.Project
-import com.intellij.openapi.wm.WindowManager
-import com.intellij.tasks.TaskManager
 import javax.swing.Icon
 import javax.swing.ImageIcon
 
@@ -26,13 +18,12 @@ class ResetTrackerAction(timer: TimeTracker) : IssueAction() {
 
     override fun actionPerformed(event: AnActionEvent) {
         event.whenActive {
-            val project = event.project
-
             if (myTimer.isRunning){
                 myTimer.isRunning = false
                 myTimer.isPaused = false
-                val bar = project?.let { it1 -> WindowManager.getInstance().getStatusBar(it1) }
-                bar?.removeWidget("Time Tracking Clock")
+                myTimer.recordedTime = "0"
+                myTimer.timeInMills = 0
+                myTimer.startTime = System.currentTimeMillis()
                 val trackerNote = TrackerNotification()
                 trackerNote.notify("Work timer reset", NotificationType.INFORMATION)
             } else {
