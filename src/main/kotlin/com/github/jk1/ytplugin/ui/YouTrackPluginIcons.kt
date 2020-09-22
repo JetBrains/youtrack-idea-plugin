188e581d56fdd71e692289fb11a47de9c874254b
lesslyrics <alina_boshenko@mail.ru>
2020-09-22 22:56:56 +0300

    minor

--- src/main/kotlin/com/github/jk1/ytplugin/ui/YouTrackPluginIcons.kt
+++ src/main/kotlin/com/github/jk1/ytplugin/ui/YouTrackPluginIcons.kt
@@ -7,7 +7,7 @@ object YouTrackPluginIcons {
     val YOUTRACK = IconLoader.getIcon("/icons/youtrack_16.png", YouTrackPluginIcons::class.java)  // 16x16
     val YOUTRACK_TOOL_WINDOW = IconLoader.getIcon("/icons/youtrack_13.png", YouTrackPluginIcons::class.java)  // 13x13
     val YOUTRACK_ISSUE_MARKER = IconLoader.getIcon("/icons/speedSearchPrompt.png", YouTrackPluginIcons::class.java)  // 13x13
-    val YOUTRACK_MANUAL_ADD_TIME_TRACKER = IconLoader.getIcon("/icons/add_new_dark_16.png", YouTrackPluginIcons::class.java)  // 16x16
+    val YOUTRACK_MANUAL_ADD_TIME_TRACKER = IconLoader.getIcon("/icons/add_workitem_manually_dark_16.png", YouTrackPluginIcons::class.java)  // 16x16
     val YOUTRACK_PAUSE_TIME_TRACKER = IconLoader.getIcon("/icons/time_tracker_pause_dark.png", YouTrackPluginIcons::class.java)  // 16x16
     val YOUTRACK_STOP_TIME_TRACKER = IconLoader.getIcon("/icons/time_tracker_stop_dark.png", YouTrackPluginIcons::class.java)  // 16x16
     val YOUTRACK_RESET_TIME_TRACKER = IconLoader.getIcon("/icons/time_tracker_reset_dark.png", YouTrackPluginIcons::class.java)  // 16x16
