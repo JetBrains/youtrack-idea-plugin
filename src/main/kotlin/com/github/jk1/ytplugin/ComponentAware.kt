b055e9aec3fa04c85132e34731952063a70776d7
lesslyrics <alina_boshenko@mail.ru>
2020-11-05 16:46:25 +0300

    clean up

--- src/main/kotlin/com/github/jk1/ytplugin/ComponentAware.kt
+++ src/main/kotlin/com/github/jk1/ytplugin/ComponentAware.kt
@@ -50,11 +50,6 @@ interface ComponentAware {
     val pluginApiComponent: YouTrackPluginApiService
         get() = project.getService(YouTrackPluginApiService::class.java) as YouTrackPluginApiService
 
-//    val timeTrackerStoreComponent: PersistentTimeTrackerStore
-//        get() = ApplicationManager.getApplication().getService(PersistentTimeTrackerStore::class.java)!!
-
-//    val timeTrackerUpdaterComponent: TimeTrackerStoreUpdaterService
-//        get() = project.getService(TimeTrackerStoreUpdaterService::class.java)!!
 
     val timeTrackerComponent: TimeTracker
         get() = project.getService(TimeTracker::class.java)!!
