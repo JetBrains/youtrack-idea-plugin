357f62cd7fecd59cade7ccaa19f74f1de451344c
lesslyrics <alina.boshchenko@jetbrains.com>
2021-03-07 11:00:41 +0300

    epdate to config + get rid of connection errors

--- src/main/kotlin/com/github/jk1/ytplugin/workflowsDebugConfiguration/JSRemoteWorkflowsDebugConfiguration.kt
+++ src/main/kotlin/com/github/jk1/ytplugin/workflowsDebugConfiguration/JSRemoteWorkflowsDebugConfiguration.kt
@@ -9,10 +9,7 @@ import com.intellij.execution.configurations.RunConfiguration
 import com.intellij.execution.configurations.RunProfileState
 import com.intellij.execution.runners.ExecutionEnvironment
 import com.intellij.execution.runners.RunConfigurationWithSuppressedDefaultRunAction
-import com.intellij.javascript.debugger.LocalFileSystemFileFinder
-import com.intellij.javascript.debugger.RemoteDebuggingFileFinder
-import com.intellij.javascript.debugger.createUrlToLocalMap
-import com.intellij.javascript.debugger.execution.JSLocalFilesMappingPanel
+import com.intellij.javascript.debugger.*
 import com.intellij.javascript.debugger.execution.RemoteUrlMappingBean
 import com.intellij.openapi.options.SettingsEditor
 import com.intellij.openapi.project.Project
@@ -20,10 +17,8 @@ import com.intellij.openapi.ui.VerticalFlowLayout
 import com.intellij.openapi.util.InvalidDataException
 import com.intellij.openapi.util.text.StringUtil
 import com.intellij.ui.GuiUtils
-import com.intellij.ui.HideableTitledPanel
 import com.intellij.ui.IdeBorderFactory
 import com.intellij.ui.PortField
-import com.intellij.uiDesigner.core.AbstractLayout
 import com.intellij.util.SmartList
 import com.intellij.util.ui.FormBuilder
 import com.intellij.util.xmlb.SkipEmptySerializationFilter
@@ -35,14 +30,15 @@ import com.intellij.xdebugger.XDebugProcess
 import com.intellij.xdebugger.XDebugSession
 import com.jetbrains.debugger.wip.BrowserChromeDebugProcess
 import com.jetbrains.debugger.wip.WipWithExclusiveWebsocketChannelVmConnection
+import org.apache.http.HttpHost
+import org.apache.http.conn.HttpInetSocketAddress
 import org.jdom.Element
 import org.jetbrains.debugger.DebuggableRunConfiguration
-import java.awt.BorderLayout
+import org.jetbrains.debugger.connection.VmConnection
 import java.net.InetAddress
 import java.net.InetSocketAddress
 import javax.swing.JComponent
 import javax.swing.JPanel
-
 private const val DEFAULT_PORT = 9229
 private val SERIALIZATION_FILTER = SkipEmptySerializationFilter()
 
@@ -50,12 +46,12 @@ class JSRemoteWorkflowsDebugConfiguration(project: Project, factory: Configurati
     : LocatableConfigurationBase<Element>(project, factory, name),
         RunConfigurationWithSuppressedDefaultRunAction,
         DebuggableRunConfiguration {
-
     @Attribute
     var host: String? = null
         set(value) {
-            field = value
+            field =  value
         }
+
     @Attribute
     var port: Int = DEFAULT_PORT
 
@@ -63,14 +59,6 @@ class JSRemoteWorkflowsDebugConfiguration(project: Project, factory: Configurati
     @XCollection
     var mappings: MutableList<RemoteUrlMappingBean> = SmartList()
         private set
-
-    override fun createDebugProcess(socketAddress: InetSocketAddress,
-                                    session: XDebugSession,
-                                    executionResult: ExecutionResult?,
-                                    environment: ExecutionEnvironment): XDebugProcess {
-        return createWipDebugProcess(socketAddress, session, executionResult)
-    }
-
 
     override fun getConfigurationEditor(): SettingsEditor<out RunConfiguration> {
         return WipRemoteDebugConfigurationSettingsEditor()
@@ -105,15 +93,18 @@ class JSRemoteWorkflowsDebugConfiguration(project: Project, factory: Configurati
     }
 
     override fun computeDebugAddress(state: RunProfileState): InetSocketAddress {
-        return InetSocketAddress(host, port)
+        return host?.let {
+            HttpInetSocketAddress(HttpHost(it),InetAddress.getLoopbackAddress(), port)
+        } ?: HttpInetSocketAddress(null, InetAddress.getLoopbackAddress(), port)
     }
 
-//    override fun computeDebugAddress(state: RunProfileState): InetSocketAddress {
-//        return host?.let {
-//            HttpInetSocketAddress(it, port)
-//        } ?: HttpInetSocketAddress(InetAddress.getLoopbackAddress(), port)
-//    }
-
+    override fun createDebugProcess(socketAddress: InetSocketAddress,
+                                    session: XDebugSession,
+                                    executionResult: ExecutionResult?,
+                                    environment: ExecutionEnvironment): XDebugProcess {
+        val debugProcess: JavaScriptDebugProcess<VmConnection<*>> = createWipDebugProcess(socketAddress, session, executionResult)
+        return debugProcess
+    }
 
     private fun createWipDebugProcess(socketAddress: InetSocketAddress,
                                       session: XDebugSession,
@@ -130,7 +121,7 @@ class JSRemoteWorkflowsDebugConfiguration(project: Project, factory: Configurati
         private val hostField = GuiUtils.createUndoableTextField()
         private val portField = PortField(DEFAULT_PORT, 1024)
 //        private val filesMappingPanel: JSLocalFilesMappingPanel
-
+//
 //        init {
 //            filesMappingPanel = object : JSLocalFilesMappingPanel(project, BorderLayout()) {
 //                override fun initUI() {
@@ -156,7 +147,7 @@ class JSRemoteWorkflowsDebugConfiguration(project: Project, factory: Configurati
             val protocolPanel = JPanel(VerticalFlowLayout())
             protocolPanel.border = IdeBorderFactory.createTitledBorder("Attach to")
 //            filesMappingPanel.initUI()
-//            val mappingsPanel = HideableTitledPanel("Remote URLs of local files (optional)", filesMappingPanel, true)
+//            val mappingsPanel = HideableTitledPanel("Mapping", filesMappingPanel, true)
             return FormBuilder.createFormBuilder()
                     .addLabeledComponent("Host", hostField)
                     .addLabeledComponent("Port", portField)
