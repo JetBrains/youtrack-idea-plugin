0c8b54e25681a94503ab70bda079eccd9927b5dc
lesslyrics <alina.boshchenko@jetbrains.com>
2021-03-15 13:00:35 +0300

    #161 #157 fix

--- src/main/kotlin/com/github/jk1/ytplugin/issues/model/Attachment.kt
+++ src/main/kotlin/com/github/jk1/ytplugin/issues/model/Attachment.kt
@@ -4,7 +4,10 @@ import com.google.gson.JsonElement
 
 class Attachment(item: JsonElement, repoUrl: String) {
 
+    private val tmpUrl: String = item.asJsonObject.get("url").asString
+
     val fileName: String = item.asJsonObject.get("name").asString
 
-    val url: String = repoUrl.substring(0, repoUrl.length - 9) + item.asJsonObject.get("url").asString
+    val url: String = repoUrl + if (tmpUrl.contains("/youtrack"))
+        tmpUrl.split("/youtrack")[1] else tmpUrl
 }
