f0095553549af08887c489d6b673aa3964eedfc1
lesslyrics <alina.boshchenko@jetbrains.com>
2021-05-06 12:08:42 +0300

    migrating to apache client 4.x

--- src/main/kotlin/com/github/jk1/ytplugin/rest/WorkflowsRestClient.kt
+++ src/main/kotlin/com/github/jk1/ytplugin/rest/WorkflowsRestClient.kt
@@ -4,65 +4,47 @@ import com.github.jk1.ytplugin.issues.model.Workflow
 import com.github.jk1.ytplugin.issues.model.WorkflowRule
 import com.github.jk1.ytplugin.logger
 import com.github.jk1.ytplugin.tasks.YouTrackServer
-import com.github.jk1.ytplugin.timeTracker.TrackerNotification
-import com.google.gson.JsonArray
 import com.google.gson.JsonObject
-import com.google.gson.JsonParser
-import com.intellij.notification.NotificationType
-import org.apache.commons.httpclient.NameValuePair
-import org.apache.commons.httpclient.methods.GetMethod
+import org.apache.http.client.methods.HttpGet
+import org.apache.http.client.utils.URIBuilder
 
 class WorkflowsRestClient(override val repository: YouTrackServer) : RestClientTrait, ResponseLoggerTrait {
 
     fun getWorkflowWithRules(workflowName: String): Workflow? {
-        val getWorkflowsListUrl = "${repository.url}/api/admin/workflows"
-        val method = GetMethod(getWorkflowsListUrl)
-        method.params.contentCharset = "UTF-8"
 
-        val top = NameValuePair("\$top", "-1")
-        val fields = NameValuePair("fields", "name,id,rules(id,name)")
-        val query = NameValuePair("query", "language:JS,system:null")
-        method.setQueryString(arrayOf(top, fields, query))
+        val builder = URIBuilder("${repository.url}/api/admin/workflows")
+        builder.setParameter("\$top", "-1")
+                .setParameter("fields", "name,id,rules(id,name)")
+                .setParameter("query", "language:JS,system:null")
+        val method = HttpGet(builder.build())
 
-        return method.connect {
+        return method.execute { element ->
             var workflow: Workflow? = null
-            when (val status = httpClient.executeMethod(method)) {
-                200 -> {
-                    logger.debug("Successfully fetched workflow rules list: code $status")
-                    val jsonArray: JsonArray = JsonParser.parseString(method.responseBodyAsString) as JsonArray
-                    for (json in jsonArray){
-                        if (json.asJsonObject.get("name").asString.contains(workflowName)){
-                            workflow = Workflow(json as JsonObject)
-                            break
-                        }
-                    }
-                    logger.warn("Workflow not found: ${method.responseBodyAsLoggedString()}")
-                    workflow
-                }
-                else -> {
-                    logger.warn("failed to fetch workflow rules: ${method.responseBodyAsLoggedString()}")
-                    throw RuntimeException()
+            val jsonArray = element.asJsonArray
+            for (json in jsonArray) {
+                if (json.asJsonObject.get("name").asString.contains(workflowName)) {
+                    workflow = Workflow(json as JsonObject)
+                    break
                 }
             }
+            logger.warn("Workflow not found: ${element.asString}")
+            workflow
         }
+//     logger.warn("failed to fetch workflow rules: ${method.responseBodyAsLoggedString()}")
     }
 
 
     fun getWorkFlowContent(workflow: Workflow, rule: WorkflowRule) {
-        val url = "${repository.url}/api/admin/workflows/${workflow.id}/rules/${rule.id}"
-        val myFields = NameValuePair("fields", "script")
-        val myTop = NameValuePair("\$top", "-1")
-        val method = GetMethod(url)
-        method.setQueryString(arrayOf(myTop, myFields))
-        return method.connect {
-            when (val status = httpClient.executeMethod(method)) {
-                200 -> {
-                    rule.content = JsonParser.parseString(method.responseBodyAsString).asJsonObject.get("script").asString
-                }
-                else -> {
-                    logger.warn("Unable to get workFlow content $status: ${method.responseBodyAsLoggedString()}")
-                }
-            }
+
+        val builder = URIBuilder("${repository.url}/api/admin/workflows/${workflow.id}/rules/${rule.id}")
+        builder.setParameter("\$top", "-1")
+                .setParameter("fields", "script")
+        val method = HttpGet(builder.build())
+
+        return method.execute { element ->
+            rule.content =  element.asJsonObject.get("script").asString
         }
+//        logger.warn("Unable to get workFlow content $status: ${method.responseBodyAsLoggedString()}")
+
     }
 }
