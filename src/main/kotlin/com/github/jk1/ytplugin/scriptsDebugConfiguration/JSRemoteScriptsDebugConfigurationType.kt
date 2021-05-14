b4995b2d63cdf3a32f1de5a03c51102daf44100d
lesslyrics <alina.boshchenko@jetbrains.com>
2021-05-14 17:26:45 +0300

    removed unnecessary type info

--- src/main/kotlin/com/github/jk1/ytplugin/scriptsDebugConfiguration/JSRemoteScriptsDebugConfigurationType.kt
+++ src/main/kotlin/com/github/jk1/ytplugin/scriptsDebugConfiguration/JSRemoteScriptsDebugConfigurationType.kt
@@ -1,26 +1,39 @@
 package com.github.jk1.ytplugin.scriptsDebugConfiguration
 
 import com.intellij.execution.RunConfigurationConverter
+import com.intellij.execution.configuration.ConfigurationFactoryEx
 import com.intellij.execution.configurations.ConfigurationFactory
 import com.intellij.execution.configurations.ConfigurationTypeBase
 import com.intellij.execution.configurations.RunConfigurationSingletonPolicy
 import com.intellij.icons.AllIcons
+import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.NotNullLazyValue
+import com.intellij.remoteServer.ServerType
+import com.intellij.remoteServer.configuration.RemoteServersManager
+import com.intellij.remoteServer.configuration.deployment.DeploymentConfigurator
+import com.intellij.remoteServer.impl.configuration.deployment.DeployToServerRunConfiguration
+import com.intellij.util.ArrayUtil
 import org.jdom.Element
+import org.jetbrains.annotations.NotNull
 
 private const val ID = "WorkflowsRemoteDebugType"
 private const val FACTORY_ID = "Workflows Remote Debug"
 
-class JSRemoteScriptsDebugConfigurationType
-    : ConfigurationTypeBase(ID, "Remote Debug Of Workflows Scripts", null, NotNullLazyValue.createValue { AllIcons.Actions.IntentionBulbGrey }),
-        DumbAware, RunConfigurationConverter {
+class JSRemoteScriptsDebugConfigurationType : ConfigurationTypeBase(
+    ID,
+    "Remote Debug Of Workflows Scripts",
+    null,
+    NotNullLazyValue.createValue { AllIcons.Actions.IntentionBulbGrey }), DumbAware, RunConfigurationConverter {
+
     init {
         addFactory(object : ConfigurationFactory(this) {
             override fun getSingletonPolicy() = RunConfigurationSingletonPolicy.SINGLE_INSTANCE_ONLY
 
             override fun getId() = FACTORY_ID
+
+            override fun isApplicable(project: Project) = true
 
             override fun createTemplateConfiguration(project: Project) = JSRemoteScriptsDebugConfiguration(project, this, configurationTypeDescription)
 
@@ -30,31 +43,11 @@ class JSRemoteScriptsDebugConfigurationType
 
     override fun getTag() = "jsRemote"
 
-    override fun convertRunConfigurationOnDemand(element: Element): Boolean {
-        if (!isNodeJsRemoteDebugConfiguration(element)) {
-            return false
-        }
-
-        element.setAttribute("type", ID)
-        element.setAttribute("factoryName", FACTORY_ID)
-        element.setAttribute("isV8Legacy", "true")
-        val iterator = element.children.listIterator()
-        while (iterator.hasNext()) {
-            val next = iterator.next()
-            if (next.name == "node-js-remote-debug") {
-                next.getAttribute("host")?.let { element.setAttribute("host", it.value) }
-                next.getAttribute("debug-port")?.let { element.setAttribute("port", it.value) }
-                iterator.remove()
-            }
-        }
-        return true
-    }
+    override fun convertRunConfigurationOnDemand(element: Element): Boolean = false
 
     override fun getHelpTopic() = "reference.dialogs.rundebug.ChromiumRemoteDebugType"
-}
 
-private fun isNodeJsRemoteDebugConfiguration(element: Element): Boolean {
-    return "NodeJSRemoteDebug".equals(element.getAttributeValue("type"), ignoreCase = true) &&
-            // skip default to not disturb people
-            "true" != element.getAttributeValue("default")
+     fun update(event: AnActionEvent) {
+        event.presentation.isEnabledAndVisible = event.project != null
+    }
 }
