baf5de74621d63463ee0f27713eda1f5b8976e7c
lesslyrics <alina.boshchenko@jetbrains.com>
2021-05-24 17:48:31 +0300

    #JT-64129 handled all input cases

--- src/main/kotlin/com/github/jk1/ytplugin/setup/ConnectionChecker.kt
+++ src/main/kotlin/com/github/jk1/ytplugin/setup/ConnectionChecker.kt
@@ -13,63 +13,61 @@ import org.apache.http.util.EntityUtils
 import java.nio.charset.StandardCharsets
 import java.util.*
 
+import org.apache.http.client.config.RequestConfig
+import org.apache.http.conn.ConnectTimeoutException
+import java.net.UnknownHostException
+
 
 class ConnectionChecker(val repository: YouTrackRepository, project: Project) {
 
     private var onSuccess: (method: HttpRequest) -> Unit = {}
 
-    private var onApplicationError: (request: HttpRequest, response: HttpResponse?) -> Unit = { _: HttpRequest, _: HttpResponse? ->}
+    private var onInputError: (request: HttpRequest) -> Unit = { _: HttpRequest -> }
 
     private var onTransportError: (request: HttpRequest, e: Exception) -> Unit = { _: HttpRequest, _: Exception -> }
     private val credentialsChecker = ComponentAware.of(project).credentialsCheckerComponent
-
-    private var isApplicationErrorFixed = false
-    private var isTransportErrorFixed = false
 
     private val String.b64Encoded: String
         get() = Base64.getEncoder().encodeToString(this.toByteArray(StandardCharsets.UTF_8))
 
 
     fun check() {
-
         logger.debug("CHECK CONNECTION FOR ${repository.url}")
         val method = HttpGet(repository.url.trimEnd('/') + "/api/users/me?fields=name")
         if (credentialsChecker.isMatchingAppPassword(repository.password) &&
-            !(credentialsChecker.isMatchingBearerToken(repository.password))){
+            !(credentialsChecker.isMatchingBearerToken(repository.password))) {
             repository.username = repository.password.split(Regex(":"), 2).first()
             repository.password = repository.password.split(Regex(":"), 2).last()
         }
         val authCredentials = "${repository.username}:${repository.password}".b64Encoded
         method.setHeader("Authorization", "Basic $authCredentials")
 
-        // TODO case with absolutely incorrect url
-        if (isApplicationErrorFixed && isTransportErrorFixed){
-            logger.debug("connection status: SUCCESS")
-            method.releaseConnection()
-            onSuccess(method)
-        }
-
         try {
             // todo: proxy
-            val response = HttpClientBuilder.create().disableRedirectHandling().build().execute(method)
-            val user = JsonParser.parseString(EntityUtils.toString(response.entity, "UTF-8")).asJsonObject.get("name").toString()
+            //timeout is required to handle connections with non-existing url looking like existing
+            val config = RequestConfig.custom().setConnectTimeout(10000).build()
+            val response = HttpClientBuilder.create().disableRedirectHandling().setDefaultRequestConfig(config).build()
+                .execute(method)
+            val user = JsonParser.parseString(EntityUtils.toString(response.entity, "UTF-8")).asJsonObject.get("name")
+                .toString()
             if (response.statusLine.statusCode == 200 && user != "\"guest\"") {
                 logger.debug("connection status: SUCCESS")
                 method.releaseConnection()
                 onSuccess(method)
             }
+
+        } catch (e: ConnectTimeoutException) {
+            logger.debug("connection status: TIMEOUT ERROR")
+            method.releaseConnection()
+            onInputError(method)
+        } catch (e: UnknownHostException) {
+            logger.debug("connection status: HOST ERROR")
+            method.releaseConnection()
+            onInputError(method)
         } catch (e: Exception) {
-            if (isApplicationErrorFixed) {
-                logger.debug("connection status: TRANSPORT ERROR")
-                method.releaseConnection()
-                isTransportErrorFixed = true
-                onTransportError(method, e)
-            } else {
-                logger.debug("connection status: APPLICATION ERROR")
-                method.releaseConnection()
-                isApplicationErrorFixed = true
-                onApplicationError(method, null)
-            }
+            logger.debug("connection status: TRANSPORT ERROR")
+            method.releaseConnection()
+            onTransportError(method, e)
         }
     }
 
@@ -77,8 +75,8 @@ class ConnectionChecker(val repository: YouTrackRepository, project: Project) {
         this.onSuccess = closure
     }
 
-    fun onApplicationError(closure: (request: HttpRequest, httpResponse: HttpResponse?) -> Unit) {
-        this.onApplicationError = closure
+    fun onInputError(closure: (request: HttpRequest) -> Unit) {
+        this.onInputError = closure
     }
 
     fun onTransportError(closure: (request: HttpRequest, e: Exception) -> Unit) {
