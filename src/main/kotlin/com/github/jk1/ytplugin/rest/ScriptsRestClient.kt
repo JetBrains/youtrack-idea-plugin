90c44b8a725b80e40dbc7bdd4119379c5d421088
lesslyrics <alina.boshchenko@jetbrains.com>
2021-06-02 19:02:09 +0300

    error handling

--- src/main/kotlin/com/github/jk1/ytplugin/rest/ScriptsRestClient.kt
+++ src/main/kotlin/com/github/jk1/ytplugin/rest/ScriptsRestClient.kt
@@ -2,8 +2,11 @@ package com.github.jk1.ytplugin.rest
 
 import com.github.jk1.ytplugin.issues.model.Workflow
 import com.github.jk1.ytplugin.issues.model.WorkflowRule
+import com.github.jk1.ytplugin.logger
 import com.github.jk1.ytplugin.tasks.YouTrackServer
+import com.github.jk1.ytplugin.timeTracker.TrackerNotification
 import com.google.gson.JsonObject
+import com.intellij.notification.NotificationType
 import org.apache.http.client.methods.HttpGet
 import org.apache.http.client.utils.URIBuilder
 
@@ -13,18 +16,27 @@ class ScriptsRestClient(override val repository: YouTrackServer) : RestClientTra
 
         val builder = URIBuilder("${repository.url}/api/admin/workflows")
         builder.setParameter("\$top", "-1")
-                .setParameter("fields", "name,id,rules(id,name)")
-                .setParameter("query", "language:JS,system:null")
+            .setParameter("fields", "name,id,rules(id,name)")
+            .setParameter("query", "language:JS,system:null")
         val method = HttpGet(builder.build())
 
         val scriptsList = mutableListOf<Workflow>()
-        return method.execute { element ->
-            val jsonArray = element.asJsonArray
-            for (json in jsonArray) {
-                scriptsList.add(Workflow(json as JsonObject))
+        return try {
+            method.execute { element ->
+
+                val jsonArray = element.asJsonArray
+                for (json in jsonArray) {
+                    scriptsList.add(Workflow(json as JsonObject))
+                }
+                scriptsList
             }
-            scriptsList
+        } catch (e: RuntimeException) {
+            logger.debug("Unable to load YouTrack Scripts: ${e.message}")
+            val trackerNote = TrackerNotification()
+            trackerNote.notify("Connection to the YouTrack might be lost", NotificationType.WARNING)
+            emptyList()
         }
+
     }
 
 
@@ -32,11 +44,11 @@ class ScriptsRestClient(override val repository: YouTrackServer) : RestClientTra
 
         val builder = URIBuilder("${repository.url}/api/admin/workflows/${workflow.id}/rules/${rule.id}")
         builder.setParameter("\$top", "-1")
-                .setParameter("fields", "script")
+            .setParameter("fields", "script")
         val method = HttpGet(builder.build())
 
         return method.execute { element ->
-            rule.content =  element.asJsonObject.get("script").asString
+            rule.content = element.asJsonObject.get("script").asString
         }
 //        logger.warn("Unable to get workFlow content $status: ${method.responseBodyAsLoggedString()}")
 
