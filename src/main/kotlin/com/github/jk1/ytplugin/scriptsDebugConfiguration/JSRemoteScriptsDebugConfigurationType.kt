e007703f691ce3e6bf8110c7b5ea2a168468a0bf
lesslyrics <alina.boshchenko@jetbrains.com>
2021-06-21 20:01:17 +0300

    compatibility for the community edition #JT-64728

--- src/main/kotlin/com/github/jk1/ytplugin/scriptsDebugConfiguration/JSRemoteScriptsDebugConfigurationType.kt
+++ src/main/kotlin/com/github/jk1/ytplugin/scriptsDebugConfiguration/JSRemoteScriptsDebugConfigurationType.kt
@@ -1,9 +1,11 @@
 package com.github.jk1.ytplugin.scriptsDebugConfiguration
 
+import com.github.jk1.ytplugin.logger
 import com.intellij.execution.configurations.ConfigurationFactory
 import com.intellij.execution.configurations.ConfigurationTypeBase
 import com.intellij.execution.configurations.RunConfigurationSingletonPolicy
 import com.intellij.icons.AllIcons
+import com.intellij.openapi.application.ApplicationNamesInfo
 import com.intellij.openapi.extensions.ExtensionNotApplicableException
 import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
@@ -20,17 +22,25 @@ class JSRemoteScriptsDebugConfigurationType : ConfigurationTypeBase(
 ), DumbAware {
 
     init {
+        val edition = ApplicationNamesInfo.getInstance().editionName
+
         if (Registry.`is`("youtrack.script.debug", false)) {
-            addFactory(object : ConfigurationFactory(this) {
-                override fun getSingletonPolicy() = RunConfigurationSingletonPolicy.SINGLE_INSTANCE_ONLY
+            if (edition != "Community Edition"){
+                logger.debug("IDE edition: $edition")
+                addFactory(object : ConfigurationFactory(this) {
+                    override fun getSingletonPolicy() = RunConfigurationSingletonPolicy.SINGLE_INSTANCE_ONLY
 
-                override fun getId() = FACTORY_ID
+                    override fun getId() = FACTORY_ID
 
-                override fun createTemplateConfiguration(project: Project) =
-                    JSRemoteScriptsDebugConfiguration(project, this, configurationTypeDescription)
+                    override fun createTemplateConfiguration(project: Project) =
+                        JSRemoteScriptsDebugConfiguration(project, this, configurationTypeDescription)
 
-                override fun isEditableInDumbMode() = true
-            })
+                    override fun isEditableInDumbMode() = true
+                })
+            } else {
+                logger.debug("IDE edition is not compatible with scripts debugger: $edition")
+            }
+
         } else {
             throw ExtensionNotApplicableException.INSTANCE
         }
