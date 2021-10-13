bb5f49936032e1b1eeefcf9fe6e8f057abf89892
Evgeny Naumenko <Evgeny.Naumenko@jetbrains.com>
2021-10-13 13:16:16 +0300

    EAP tests fixed

--- src/test/kotlin/com/github/jk1/ytplugin/workItems/TimerWidgetTest.kt
+++ src/test/kotlin/com/github/jk1/ytplugin/workItems/TimerWidgetTest.kt
@@ -4,16 +4,13 @@ import com.github.jk1.ytplugin.ComponentAware
 import com.github.jk1.ytplugin.IdeaProjectTrait
 import com.github.jk1.ytplugin.timeTracker.TimerWidget
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.wm.WindowManager
 import com.intellij.openapi.wm.impl.TestWindowManager
 import com.intellij.testFramework.fixtures.IdeaProjectTestFixture
 import org.junit.After
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
 import java.util.concurrent.TimeUnit
-import kotlin.jvm.internal.Intrinsics
-import kotlin.test.assertNotEquals
-
 
 class InputCredentialsTest :  IdeaProjectTrait, ComponentAware {
 
@@ -24,30 +21,23 @@ class InputCredentialsTest :  IdeaProjectTrait, ComponentAware {
     fun setUp() {
         fixture = getLightCodeInsightFixture()
         fixture.setUp()
-        val windowManager: WindowManager = TestWindowManager()
-        val statusBar = windowManager.getStatusBar(project)
-        val myTimer = timeTrackerComponent
-
-        myTimer.isRunning = true
-        myTimer.isPaused = false
-        if (statusBar?.getWidget("Time Tracking Clock") == null) {
-            statusBar?.addWidget(TimerWidget(myTimer, project), project)
+        val statusBar = TestWindowManager().getStatusBar(project)
+        val timer = timeTrackerComponent
+        timer.isRunning = true
+        timer.isPaused = false
+        if (statusBar.getWidget("Time Tracking Clock") == null) {
+            statusBar.addWidget(TimerWidget(timer, project), project)
         }
     }
 
     @Test
     fun testTimeChanges() {
-        val windowManager: WindowManager = TestWindowManager()
-        val statusBar = windowManager.getStatusBar(project)
-        val widget: TimerWidget? = statusBar.getWidget("Time Tracking Clock") as TimerWidget?
-        if (widget == null) {
-            Intrinsics.throwNpe()
-        } else {
-            val oldTime: String = widget.time()
-            TimeUnit.MINUTES.sleep(1L)
-            val currentTime: String = widget.time()
-            assertNotEquals(oldTime, currentTime, "Time is constant")
-        }
+        val statusBar = TestWindowManager().getStatusBar(project)
+        val widget = statusBar.getWidget("Time Tracking Clock") as TimerWidget
+        val oldTime = widget.time()
+        TimeUnit.MINUTES.sleep(1L)
+        val currentTime = widget.time()
+        Assert.assertNotEquals(oldTime, currentTime, "Time is constant")
     }
 
     @After
