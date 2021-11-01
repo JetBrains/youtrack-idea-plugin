0d0719c6e35d36724d921d014438abe1e664c069
lesslyrics <alina.boshchenko@jetbrains.com>
2021-11-01 19:27:13 +0300

    refactoring

--- src/main/kotlin/com/github/jk1/ytplugin/scriptsDebug/WipConnection.kt
+++ src/main/kotlin/com/github/jk1/ytplugin/scriptsDebug/WipConnection.kt
@@ -64,6 +64,7 @@ open class WipConnection(val project: Project) : RemoteVmConnection<WipVm>() {
     private var webSocketDebuggerEndpoint: String? = null
     private var webSocketPrefix: String? = null
 
+    private val REMOTE_SOURCE_MSG = "{\"result\":{\"scriptSource\""
     val logger: Logger get() = Logger.getInstance("com.github.jk1.ytplugin")
 
     companion object {
@@ -344,7 +345,7 @@ open class WipConnection(val project: Project) : RemoteVmConnection<WipVm>() {
             WebSocketFrameAggregator(NettyUtil.MAX_CONTENT_LENGTH),
             object : WebSocketProtocolHandler() {
                 override fun textFrameReceived(channel: Channel, message: TextWebSocketFrame) {
-                    if (message.text().contains("{\"result\":{\"scriptSource\"")){
+                    if (message.text().contains(REMOTE_SOURCE_MSG)){
                         vm.textFrameReceived(TextWebSocketFrame(ScriptsRulesHandler(project)
                             .handleScriptsSourcesMessages(message.text())))
                     } else {
